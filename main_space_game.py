246791826786e6034077274a7395bd811b65d30c
voland2610 <vvoland2610@gmail.com>
2022-04-26 19:59:29 +0300

    Добавление фонового изображения

--- main_space_game.py
+++ main_space_game.py
@@ -6,14 +6,14 @@ def start():
     # Базовые настройки экрана игры
     pygame.init()
     pygame.display.set_caption("Space war")
-    backgroundColor = (0, 0, 0)
-    display = pygame.display.set_mode((1200, 800))
+    backgroundimg = pygame.image.load("img/Purple Nebula 8 - 1024x1024.png")
+    display = pygame.display.set_mode((1024, 1024))
     # Основной цикл игры
     while True:
         for events in pygame.event.get():
             if events.type == pygame.QUIT:
                 sys.exit()
-        display.fill(backgroundColor)
+        display.blit(backgroundimg, (0, 0))
         pygame.display.flip()
 
 
