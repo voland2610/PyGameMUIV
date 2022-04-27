ad1a148b591442913960c8099c32c64cebd06e72
voland2610 <vvoland2610@gmail.com>
2022-04-28 02:19:52 +0300

    Добавленно стартовое меню

--- main_space_game.py
+++ main_space_game.py
@@ -1,14 +1,22 @@
-import pygame
 from shipGun import ShipGun
 import events_control
+import pygame
+import pygame_menu
+from pygame_menu.examples import create_example_window
+from typing import Tuple, Any
+
+surface = create_example_window('Example - Simple', (600, 400))
 
 
-def start():
-    # Базовые настройки экрана игры
+def set_difficulty(selected: Tuple, value: Any):
+    pass
+
+
+def start_the_game():
     pygame.init()
     pygame.display.set_caption("Space war")
     backgroundImg = pygame.image.load("img/bg/Purple Nebula 8 - 1024x1024.png")
-    display = pygame.display.set_mode((1920, 1000))
+    display = pygame.display.set_mode((1920, 1000), pygame.RESIZABLE)
     shipGun = ShipGun(display)
     container_bullet = pygame.sprite.Group()
 
@@ -20,7 +28,16 @@ def start():
         events_control.display_update(backgroundImg, display, shipGun, container_bullet)
 
 
+menu_start_game = pygame_menu.Menu(
+    height=300,
+    theme=pygame_menu.themes.THEME_BLUE,
+    title='Welcome',
+    width=400
+)
 
+#menu.add.selector('Difficulty: ', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
+menu_start_game.add.button('Play', start_the_game)
+menu_start_game.add.button('Quit', pygame_menu.events.EXIT)
 
-
-start()
+if __name__ == '__main__':
+    menu_start_game.mainloop(surface)
